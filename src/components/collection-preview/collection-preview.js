cbf92dccf707a751702fc15a94eca2cd7b3fd193
simoncobb1966 <48588635+simoncobb1966@users.noreply.github.com>
2021-05-27 16:49:17 +0100

    updating with shop page, preview & collection items

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -1,4 +1,5 @@
 import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
@@ -6,8 +7,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, index) => index < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>

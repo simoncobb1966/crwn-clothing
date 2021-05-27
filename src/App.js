2d2bdc00026a10f7377462c1716474f26b83d7e0
simoncobb1966 <48588635+simoncobb1966@users.noreply.github.com>
2021-05-27 14:28:58 +0100

    Routing now working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "../src/pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory-component.js
+++ src/components/directory/directory-component.js
@@ -14,7 +14,7 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: "shop/hats",
+          linkUrl: "hats",
         },
         {
           title: "jackets",
@@ -49,13 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem
-            key={id}
-            size={size}
-            title={title.toUpperCase()}
-            imageUrl={imageUrl}
-          />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );

ff37de27c32884e0fed41fe422a01536af799940
simoncobb1966 <48588635+simoncobb1966@users.noreply.github.com>
2021-05-28 10:31:26 +0100

    sign in page completed

---
+++ src/components/sign-in/sign-in.component.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import FormInput from "../../components/form-input/form-input.component";
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            handleChange={this.handleChange}
+            name="email"
+            type="email"
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <FormInput
+            handleChange={this.handleChange}
+            name="password"
+            type="password"
+            value={this.state.password}
+            label="password"
+            required
+          />
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;

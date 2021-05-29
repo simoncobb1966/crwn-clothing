37ca71b88fea803bd31791ab0289f74db9121f4e
simoncobb1966 <48588635+simoncobb1966@users.noreply.github.com>
2021-05-29 17:51:51 +0100

    implemented firebase utils, included the ability to enter authenticated users to our database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,34 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyBUmxn7odng8i835_uwmsHqkB5o7itaNVw",
-  authDomain: "crwn-db-c2b4b.firebaseapp.com",
-  projectId: "crwn-db-c2b4b",
-  storageBucket: "crwn-db-c2b4b.appspot.com",
-  messagingSenderId: "212938624164",
-  appId: "1:212938624164:web:cf72a0343d4b5243dc2a31",
-  measurementId: "G-JEXQTRYY51",
+  apiKey: "AIzaSyC5A3CuuTmmTeOjYSr5s6viVXerQfwe8Ow",
+  authDomain: "crwn1-db-5d185.firebaseapp.com",
+  projectId: "crwn1-db-5d185",
+  storageBucket: "crwn1-db-5d185.appspot.com",
+  messagingSenderId: "781798674215",
+  appId: "1:781798674215:web:ccb4398561b14e0e826302",
+  measurementId: "G-X4YTL9S1LM",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,11 @@
 import SignIn from "../../components/sign-in/sign-in.component.js";
+import SignUp from "../../components/sign-up/sign-up.component.js";
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
